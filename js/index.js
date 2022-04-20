7016645da2295839ef13d1680bebfec5867da526
need <82262924+needdd@users.noreply.github.com>
2022-04-20 19:33:01 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    $('.stages_none').slideDown(1000)
+setInterval(function() {
+        $('.stages').css("animation","ainfff 2s forwards")
+    },2000);
+
+    // var left = 0
+    // $('.but_l').on('click',function () {
+    //     if(left > 0){
+    //         var distance = $('.subject').css("left")
+    //         distance = replace
+    //         distance = distance - 100
+    //         console.log(distance);
+    //         $('.subject').css("left","100px")
+    //         left++
+    //     } else {
+    //         return
+    //     }
+    // });
+    // $('.but_r').on('click',function () {
+    //     if(left <= 0){
+    //         var distance = $('.subject').css("left")
+    //         console.log(distance);
+    //         $('.subject').css("left","-100px")
+    //         left++
+    //     } else {
+    //         return
+    //     }
+    // })
+
+}
+
+    
